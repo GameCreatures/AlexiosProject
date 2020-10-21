d180aa72208f504dce90f34066a287879f6b97a7
DESKTOP-G3OJRFP\ARive <a.rivera.9612@outlook.com>
2020-10-21 07:25:43 -0400

    FirstCommit

---
+++ Assets/Scripts/GamepadPlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamepadPlayerController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public float speed = 6.0F;
+	public float gravity = 20.0F;
+
+	private Vector3 moveDirection = Vector3.zero;
+	public CharacterController controller;
+
+	private Vector3 rotation;
+	void Start()
+	{
+		// Store reference to attached component
+		controller = GetComponent<CharacterController>();
+	}
+
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (moveDirection.magnitude > 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+          
+
+            controller.Move(moveDirection * speed * Time.deltaTime);
+        }
+
+        UpdateAnimator();
+    }
+
+
+
+    private void UpdateAnimator()
+	{
+
+		Vector3 localVelocity = transform.InverseTransformDirection(moveDirection);
+		
+
+		float speed = localVelocity.z * 4;
+		GetComponent<Animator>().SetFloat("forwardSpeed", Mathf.Abs(speed));
+	}
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+
+    Ray lastRay;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHIt = Physics.Raycast(ray, out hit);
+
+
+        if (hasHIt)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+}
